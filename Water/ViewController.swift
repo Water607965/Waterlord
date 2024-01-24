c2f494f1e76d3bb95600c7046242fe8e2ffb309d
Water <water@h2owater.xyz>
2024-01-24 15:01:02 -0500

    Open Source App

---
+++ Water/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Water
+//
+//  Created by Water on 1/23/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
